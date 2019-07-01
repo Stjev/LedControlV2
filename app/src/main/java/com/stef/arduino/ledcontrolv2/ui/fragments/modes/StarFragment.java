81f926e9aff76ee996459c489c4663e99d7f031e
Stef De Visscher <sddvissc.devisscher@ugent.be>
2019-07-01 20:40:05 +0200

    star mode implemented

--- app/src/main/java/com/stef/arduino/ledcontrolv2/ui/fragments/modes/StarFragment.java
+++ app/src/main/java/com/stef/arduino/ledcontrolv2/ui/fragments/modes/StarFragment.java
@@ -1,30 +1,74 @@
 package com.stef.arduino.ledcontrolv2.ui.fragments.modes;
 
 
+import android.arch.lifecycle.ViewModelProviders;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
 
 import com.stef.arduino.ledcontrolv2.R;
+import com.stef.arduino.ledcontrolv2.databinding.FragmentStarBinding;
+import com.stef.arduino.ledcontrolv2.viewmodels.StarViewModel;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class StarFragment extends Fragment {
 
+    private StarViewModel starViewModel;
 
     public StarFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        FragmentStarBinding binding =
+                DataBindingUtil.inflate(inflater, R.layout.fragment_star, container, false);
+
+        starViewModel = ViewModelProviders.of(this).get(StarViewModel.class);
+
+        // Set the bindings
+        binding.setSpeed(starViewModel.getStarSpeed().getValue());
+        binding.setCount(starViewModel.getStarCount().getValue());
+        binding.setCountChanged((SeekBar _s, int count, boolean _fu) -> binding.setCount(count));
+        binding.setSpeedChanged((SeekBar _s, int speed, boolean _fu) -> binding.setSpeed(speed));
+        binding.setCountListener(countListener);
+        binding.setSpeedListener(speedListener);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_star, container, false);
+        return binding.getRoot();
     }
 
+    /**
+     * This will control the changes in the speed seekbar
+     */
+    private SeekBar.OnSeekBarChangeListener speedListener = new SeekBar.OnSeekBarChangeListener() {
+        @Override public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {}
+        @Override public void onStartTrackingTouch(SeekBar seekBar) {}
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+            if(starViewModel != null)
+                starViewModel.setStarSpeed(seekBar.getProgress());
+        }
+    };
+
+    /**
+     * This will control the changes in the star count seekbar
+     */
+    private SeekBar.OnSeekBarChangeListener countListener = new SeekBar.OnSeekBarChangeListener() {
+        @Override public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {}
+        @Override public void onStartTrackingTouch(SeekBar seekBar) {}
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+            if(starViewModel != null)
+                starViewModel.setStarCount(seekBar.getProgress());
+        }
+    };
 }
--- app/src/main/java/com/stef/arduino/ledcontrolv2/viewmodels/StarViewModel.java
+++ app/src/main/java/com/stef/arduino/ledcontrolv2/viewmodels/StarViewModel.java
@@ -1,6 +1,33 @@
 package com.stef.arduino.ledcontrolv2.viewmodels;
 
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 
 public class StarViewModel extends ViewModel {
+    private MutableLiveData<Integer> starCount;
+    private MutableLiveData<Integer> starSpeed;
+
+    public StarViewModel() {
+        starCount = new MutableLiveData<>();
+        starSpeed = new MutableLiveData<>();
+        starCount.setValue(15); // Default value is 15
+        starSpeed.setValue(50); // Default value is 50%
+    }
+
+    public LiveData<Integer> getStarCount() {
+        return this.starCount;
+    }
+
+    public LiveData<Integer> getStarSpeed() {
+        return this.starSpeed;
+    }
+
+    public void setStarCount(int starCount) {
+        this.starCount.setValue(starCount);
+    }
+
+    public void setStarSpeed(int starSpeed) {
+        this.starSpeed.setValue(starSpeed);
+    }
 }
